4261e634e9eb7a866ea71c46509a7a2365553ded
Headerdev <headerdev@gmail.com>
2023-07-02 13:11:20 +0700

    Storing the user in the memory

--- src/main/java/com/idevalot/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/idevalot/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,12 +18,21 @@ public class BasicAuthSecurityConfiguration {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated());
 		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//		http.formLogin(withDefaults());
+
 		http.httpBasic(withDefaults());
 		http.csrf(csrf -> csrf.disable());
-		
-		
+
 		return http.build();
+	}
+
+	@Bean
+	UserDetailsService userDetailService() {
+
+		var user = User.withUsername("idevalot").password("{noop}dummy").roles("USER").build();
+
+		var admin = User.withUsername("admin").password("{noop}dummy").roles("ADMIN").build();
+
+		return new InMemoryUserDetailsManager(user, admin);
 	}
 
 }
