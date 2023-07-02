149b6ce4ac1199d671fedcc185b16c38c3b83834
Headerdev <headerdev@gmail.com>
2023-07-02 14:53:45 +0700

    Storing BCrypt Encoded Passwords

--- src/main/java/com/idevalot/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/idevalot/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -33,36 +34,35 @@ public class BasicAuthSecurityConfiguration {
 		return http.build();
 	}
 
-//	@Bean
-//	UserDetailsService userDetailService() {
-//
-//		var user = User.withUsername("idevalot").password("{noop}dummy").roles("USER").build();
-//
-//		var admin = User.withUsername("admin").password("{noop}dummy").roles("ADMIN").build();
-//
-//		return new InMemoryUserDetailsManager(user, admin);
-//	}
-
 	@Bean
 	DataSource dataSource() {
 		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
 				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION).build();
 	}
-	
-	
+
 	@Bean
 	public UserDetailsService userDetailService(DataSource dataSource) {
 
-		var user = User.withUsername("idevalot").password("{noop}dummy").roles("USER").build();
+		var user = User.withUsername("idevalot")
+				.password("dummy")
+				.passwordEncoder(str -> passwordEncoder().encode(str))
+				.roles("USER").build();
 
-		var admin = User.withUsername("admin").password("{noop}dummy")
+		var admin = User.withUsername("admin")
+				.password("dummy")
+				.passwordEncoder(str -> passwordEncoder().encode(str))
 				.roles("ADMIN", "USER").build();
 
 		var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 		jdbcUserDetailsManager.createUser(user);
 		jdbcUserDetailsManager.createUser(admin);
-		
+
 		return new InMemoryUserDetailsManager(user, admin);
 	}
-	
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 }
